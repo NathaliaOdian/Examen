cb56638b12ddd6b2b8705889873fb15d0ffb99d6
visual_community@outlook.com <visual_community@outlook.com>
2019-09-28 12:34:49 -0400

    commit nw

---
+++ Odian/Odian/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace Odian.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Odian.Models.Clown> Clowns { get; set; }
+    }
+}
